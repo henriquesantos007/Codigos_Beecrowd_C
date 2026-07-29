abbd4f1e218d661bfe660aa32266ccff955019f4
Luiz Santos <henriquesantos.sje@gmail.com>
2026-07-28 21:50:29 -0300

    feat(1065): Implementa o desafio 1065 da classe Iniciante

---
+++ INICIANTE/iniciante_1065_ParesEntreCincoNumeros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+    int entrada;
+    int contador = 0;
+ 
+    for (int i = 0; i < 5; i++){
+        scanf("%d", &entrada);
+
+        if (entrada % 2 == 0)
+            contador++;
+    }
+
+    printf("%d valores pares\n", contador);
+ 
+    return 0;
+}
